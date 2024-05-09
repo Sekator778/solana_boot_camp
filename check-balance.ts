d823aca7b2e30ac827e7868c7ff1298308d9dce7
Aleksandr Nicolaichuk <sekator778@gmail.com>
2024-05-09 19:53:34 +0200

    *create keys*check balance

---
+++ check-balance.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(`⚡️ Connected to devnet`);
+
+const publicKey = new PublicKey("C1aDCqg1N7oVf5rm8dqFeRYrqt299sV7GzsA3jnfTfz9");
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `💰 Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
